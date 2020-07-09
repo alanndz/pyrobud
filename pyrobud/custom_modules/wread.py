5ab743d62a3a3c87701858a5db4ea69f57bd8cb6
alanndz <alanmahmud0@gmail.com>
2020-07-09 19:55:37 +0800

    custom_modules: add WebReader module
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

---
+++ pyrobud/custom_modules/wread.py
@@ -0,0 +1,62 @@
+"""
+Articles Scraper Module for pyrobud userbot
+Author: yshalsager <ysh-alsager@hotmail.com>
+
+This module is used to get the content of an article without leaving Telegram.
+
+Dependencies: newspaper3k
+"""
+import re
+
+from newspaper import Article, ArticleException
+from telegraph import Telegraph
+from googletrans import Translator
+
+from pyrobud import command, module
+
+tgr = Telegraph()
+tl = Translator()
+
+class WebReader(module.Module):
+    name: str = "WebReader"
+    disabled: bool = False
+    regex: str = r'(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.' \
+                 r'[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*))'
+
+    @command.desc("Scrap article content, translate and upload to telegraph")
+    @command.alias("wread")
+    @command.usage("[url] [language]")
+    async def cmd_wreader(self, ctx: command.Context) -> str:
+        if len(ctx.input.split()) > 1:
+            lang = ctx.input.split()[1]
+        else: lang = None
+
+        url: str = ctx.input or ctx.segments[1]
+        url: str = re.search(self.regex, url).group(1)
+
+        article: Article = Article(url)
+
+        try:
+            await ctx.respond("Getting context ...")
+            article.download()
+            article.parse()
+            text = f"{article.text}"
+        except ArticleException:
+            return "Failed to scrape the article!"
+
+        if lang:
+            try:
+                await ctx.respond("Translating Text ...")
+                text = tl.translate(text, dest=lang)
+            except ValueError as err:
+                return "Error: `{}`".format(str(err))
+            text = text.text
+
+        await ctx.respond("Uploading to telegraph ...")
+        tgr.create_account(short_name='alanndz')
+        response = tgr.create_page(
+            "{}".format(article.title),
+            html_content="<p>{}!</p>".format(text.replace("\n", "<br>"))
+        )
+
+        return ('https://telegra.ph/{}'.format(response['path']))
