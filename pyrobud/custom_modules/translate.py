2a8e15202844c27e775315b5aa884c3a1580e4cb
alanndz <alanmahmud0@gmail.com>
2020-07-09 15:26:36 +0800

    custom_modules: add translate.py
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

---
+++ pyrobud/custom_modules/translate.py
@@ -0,0 +1,33 @@
+"""
+Translate
+Author: alanndz alanmahmud0@gmail.com
+"""
+
+from googletrans import Translator
+from pyrobud import command, module, util
+
+tl = Translator()
+
+class Translate(module.Module):
+    name: str="Markdowns"
+    disabled: bool= False
+
+    @command.desc("Translate")
+    @command.alias("tr")
+    @command.usage("[lang]", reply=True)
+    async def cmd_translate(self, ctx: command.Context) -> str:
+        input_text = ctx.input
+
+        status, text = await util.tg.get_text_input(ctx, input_text)
+        if not status:
+            if isinstance(text, str):
+                return text
+            return "__Unknown error.__"
+
+        detectlang = tl.detect(text)
+
+        try:
+            tekstr = tl.translate(text, dest=input_text)
+        except ValueError as err:
+            return ("Error: `{}`".format(str(err)))
+        return "Translate from `{}` to `{}`:\n```{}```".format(detectlang.lang, input_text, tekstr.text)
