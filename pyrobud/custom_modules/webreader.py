289acf0bcd6f5497d3381e53eb6d827e7208cc02
alanndz <alanmahmud0@gmail.com>
2020-07-11 10:12:13 +0800

    custom_modules: wread -> webreader
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- pyrobud/custom_modules/webreader.py
+++ pyrobud/custom_modules/webreader.py
@@ -2,6 +2,8 @@
 Articles Scraper Module for pyrobud userbot
 Author: yshalsager <ysh-alsager@hotmail.com>
 
+And Translate with specific languange and upload to telegraph
+Re-Author: alanndz <alanmahmud0@gmail.com>
 This module is used to get the content of an article without leaving Telegram.
 
 Dependencies: newspaper3k
@@ -21,9 +23,9 @@ class WebReader(module.Module):
                  r'[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*))'
 
     @command.desc("Scrap article content, translate and upload to telegraph")
-    @command.alias("wread")
-    @command.usage("[url] [language]")
-    async def cmd_wreader(self, ctx: command.Context) -> str:
+    @command.alias("wread", "wr")
+    @command.usage("[url] [language(optional)]")
+    async def cmd_webreader(self, ctx: command.Context) -> str:
         if len(ctx.input.split()) > 1:
             lang = ctx.input.split()[1]
         else: lang = None
@@ -37,7 +39,7 @@ class WebReader(module.Module):
         article: Article = Article(url)
 
         try:
-            await ctx.respond("Getting context ...")
+            await ctx.respond("Getting text ...")
             article.download()
             article.parse()
             text = f"{article.text}"
